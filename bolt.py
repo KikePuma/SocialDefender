8f6d7ffb643c35dbe1ee004d5273f8ff8749db9f
KikePuma <adventurespumas@gmail.com>
2017-03-21 01:17:58 +0100

    Bolt Update

--- bolt.py
+++ bolt.py
@@ -9,7 +9,7 @@ __author__ = "Kike Puma"
 __copyright__ = "Copyright 2007, CosasDePuma"
 __credits__ = ["KikePuma", "CosasDePuma"]
 __license__ = "GPL-3.0"
-__version__ = "1.0.1 Spout"
+__version__ = "1.0.2 Bolt"
 __maintainer__ = "KikePuma"
 __email__ = "kikefontanlorenzo@gmail.com"
 __status__ = "In development"
@@ -18,9 +18,11 @@ __status__ = "In development"
 ##       PROGRAM      ##
 ########################
 
+#Libraries and APIs
 import sys #System Threading
 import tweepy #API Twitter
-import codecs #Text encoder
+import codecs #Text Encoder
+from pymongo import MongoClient #API MongoDB
 
 #TwitterApp Keys and Access Tokens
 consumerKey = ''
@@ -28,12 +30,32 @@ consumerSecret = ''
 accessToken = ''
 accessSecret = ''
 
+#MongoDB Connection (MongoLab Used)
+uri = 'mongodb://user:pass@host:port/db'
+client = MongoClient(uri)
+db = client.get_default_database()
+collection_name = ''
+collection = db[collection_name]
+
 #Extend the class streamListener
 class spoutModule(tweepy.StreamListener):
     def on_status(self, status):
         if not hasattr(status, 'retweeted_status'): #Ignore retweets
-            print status.text.encode('utf-8') #Encode the text in UTF-8
-            
+            print "[!] " + status.source.encode('utf-8') + " <> " + status.text.encode('utf-8') #Print the Tweet in UTF-8 Encoding
+            #Create Tweet Information in JSON
+            tweet = {
+                    "feeling": "",
+                    "tweet":
+                    {
+                        "text": status.text.encode('utf-8'),
+                        "url": "https://twitter.com/" + status.user.screen_name + "/status/" + str(status.id),
+                        "user": status.user.name
+                    }
+                    };
+            #Insert the tweet info in the Collection
+            collection.insert_one(tweet)
+
+
     def on_error(self, status_code):
         if status_code == 420:
             print "Connection refused. Max number of tries. Please wait and try again"
@@ -45,7 +67,7 @@ class spoutModule(tweepy.StreamListener):
         return False
 
     def on_timeout(self):
-        print 'Timeout...'
+        print 'Timeout...'   
         return True
 
 #Catch KeyboardInterrupt (Ctrl + C)
